a95f1e6145cdbe4b6cd8f8275e2719a325ba83a9
Jonatan Andrei Haas <jonatan.haas@cwi.com.br>
2019-09-20 10:26:25 -0300

    Adicionada lógica para exibição do jogo e do menu

---
+++ js/model/CampoMinado.js
@@ -0,0 +1,7 @@
+export default class CampoMinado {
+    constructor (nivel, cor, vidas){
+        this.nivel = nivel;
+        this.cor = cor;
+        this.vidas = vidas;
+    }
+}
---
+++ js/model/Nivel.js
@@ -0,0 +1,9 @@
+export default class Nivel {
+    constructor(linhas, colunas, bombas, dicas, personalizado) {
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.bombas = bombas;
+        this.dicas = dicas;
+        this.personalizado = personalizado;
+    }
+}
