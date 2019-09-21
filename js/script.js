194ae22a662cca2293e8e01ed34a88a175aa5273
Jonatan Andrei Haas <jonatan.haas@cwi.com.br>
2019-09-20 22:39:00 -0300

    Adicionada lógica do jogo

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ var jogoIniciado = false;
 var exibeTabuleiro = false;
 var primeiraJogada = true;
 var campoMinado = null;
+var bloqueiaTabuleiroFimDeJogo = false;
 
 function iniciarJogo() {
     exibeTabuleiro = true;
+    bloqueiaTabuleiroFimDeJogo = false;
     document.getElementById('area-menu-inicial').classList.add('esconder');
     document.getElementById('area-jogo').classList.remove('esconder');
     var nivel = criarNivel(document.getElementById('nivel').value);
@@ -15,6 +17,7 @@ function iniciarJogo() {
         // Criar lógica para apagar tabela existente
     } else {
         criarCampoMinado(nivel, cor, vida);
+        jogoIniciado = true;
     }
 }
 
@@ -25,16 +28,43 @@ function retornarMenuPrincipal() {
 }
 
 function jogar(posicaoJogada) {
-    if (primeiraJogada) {
-        posicionarBombas(posicaoJogada);
-        primeiraJogada = false;
-    } else {
-
+    // Bloqueia jogada simultânea
+    if (!campoMinado.tabuleiro[posicaoJogada - 1].aberta && !bloqueiaTabuleiroFimDeJogo) {
+        var jogadaValida = true;
+        campoMinado.tabuleiro[posicaoJogada - 1].aberta = true;
+        if (primeiraJogada) {
+            campoMinado.celulasSemBombaReveladas++;
+            posicionarBombas(posicaoJogada);
+            primeiraJogada = false;
+        } else {
+            if (campoMinado.tabuleiro[posicaoJogada - 1].bomba) {
+                jogadaValida = false;
+                // Revela bomba e faz verificação se tem vidas
+                // se não tem mais vidas bloqueiaTabuleiroFimDeJogo = true;
+            } else {
+                campoMinado.celulasSemBombaReveladas++;
+                // Revela número
+            }
+        }
+        // Revela casas próximas sem bomba
+        if (jogadaValida) {
+            if (campoMinado.celulasSemBombaReveladas === campoMinado.nivel.celulasSemBomba) {
+                // Jogo acabou e existe vencedor
+                bloqueiaTabuleiroFimDeJogo = true;
+            }
+        }
     }
+    // Desbloqueia jogada
 }
 
 function posicionarBombas(posicaoJogada) {
     var posicoesBombas = sortearPosicoesComBombas(posicaoJogada);
+    for (var i = 0; i < posicoesBombas.length; i++) {
+        campoMinado.tabuleiro[posicionarBombas[i] - 1].bomba = true;
+    }
+    for (var i = 0; i < campoMinado.tabuleiro.length; i++) {
+        // if celula não está em posicoes bombas, calcula bombas próximas
+    }
     console.log(posicoesBombas);
 }
 
@@ -73,15 +103,15 @@ function criarNivel(codigo) {
 }
 
 function criarCampoMinado(nivel, cor, vidas) {
-    var body = document.getElementsByTagName('body')[0];
+    var areaJogo = document.getElementById('area-jogo');
     var tabela = document.createElement('table');
     tabela.setAttribute('border', '1');
     var tbody = document.createElement('tbody');
     var contador = 1;
     campoMinado = new CampoMinado(nivel, cor, vidas, []);
-    for (var i = 0; i < nivel.linhas; i++) {
+    for (var i = 1; i <= nivel.linhas; i++) {
         var tr = document.createElement('tr');
-        for (var j = 0; j < nivel.colunas; j++) {
+        for (var j = 1; j <= nivel.colunas; j++) {
             var td = document.createElement('td');
             td.setAttribute('id', contador);
             td.setAttribute('class', 'celula');
@@ -97,7 +127,7 @@ function criarCampoMinado(nivel, cor, vidas) {
     }
     tabela.appendChild(tbody);
     tabela.setAttribute('id', 'tabuleiro');
-    body.appendChild(tabela);
+    areaJogo.appendChild(tabela);
 }
 
 
@@ -108,6 +138,7 @@ class Nivel {
         this.bombas = bombas;
         this.dicas = dicas;
         this.personalizado = personalizado;
+        this.celulasSemBomba = (linhas * colunas) - bombas;
     }
 }
 
@@ -117,6 +148,7 @@ class CampoMinado {
         this.cor = cor;
         this.vidas = vidas;
         this.tabuleiro = tabuleiro;
+        this.celulasSemBombaReveladas = 0;
     }
 }
 
